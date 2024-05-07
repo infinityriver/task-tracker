b9200febf20e95cba2edb4eb8ea0e34491fb3279
infinityriver <infinityriver@mail.ru>
2024-05-07 23:42:49 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import CreateTasksContainer from './components/CreateTasks/CreateTasksContainer';
+import TasksUserContainer from './components/TasksUser/TasksUserContainer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+          <div className='app-wrapper-content'>
+            <Routes>
+              <Route path='/' element={<TasksUserContainer />} />
+              <Route path='/Admin' element={<CreateTasksContainer />} />
+            </Routes>
+          </div>
+            
+        </BrowserRouter>
     </div>
   );
 }
